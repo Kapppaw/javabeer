8a17cb1237a92b1491aa928ee2ca05fd067d396d
Thibault <Thibault@Thibault>
2013-11-26 21:28:31 +0100

    Ajout Beershop Facade

---
+++ JavaBeerApp/JavaBeerApp-ejb/src/java/sessionBean/BeershopFacadeLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sessionBean;
+
+import java.util.List;
+import javax.ejb.Local;
+import model.Beershop;
+
+/**
+ *
+ * @author Thibault
+ */
+@Local
+public interface BeershopFacadeLocal {
+
+    void create(Beershop beershop);
+
+    void edit(Beershop beershop);
+
+    void remove(Beershop beershop);
+
+    Beershop find(Object id);
+
+    List<Beershop> findAll();
+
+    List<Beershop> findRange(int[] range);
+
+    int count();
+    
+}
